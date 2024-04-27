528703b4aa74ce92b3a690531182cf91c3139c36
Zhuldyz <zhulya711@gmail.com>
2024-04-27 16:38:00 +0300

    Finalized code for final test

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿# pragma warning disable
 
-
 // *Задача*: Написать программу, которая из имеющегося массива строк формирует 
 // новый массив из строк, длина которых меньше, либо равна 3 символам. 
 // Первоначальный массив можно ввести с клавиатуры, либо задать на старте 
@@ -11,38 +10,23 @@
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-/*
-string[] array = new string []{"Hello", "2", "world", ";-)"};
+
+string[] array = new string []{"Hello", "2", "world", ":-)"};
 // string[] array = new string []{"1234", "1567", "-2", "computer science"};
 // string[] array = new string []{"Russia", "Denmark", "Kazan"};
+
 Console.Write($"[{string.Join(", ", array)}] -> ");
 
 string A = "";
-int count = 0;
-foreach (string element in array)
-{
-    if (element.Length <= 3)
-        {
-            A = A + element + " ";
-            count++;
-        }
-}
 
-string[] newArray = new string [count];
-
-void CreateNewArray()
-{
-    string[] elements = A.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-    for (int i = 0; i < count; i++)
-    {
-        newArray[i] = elements[i];
+for (int i = 0; i < array.GetLength(0); i++){
+        if (array[i].Length <= 3) 
+            A = A + array[i] + " ";     
     }
-}
-CreateNewArray();
-Console.WriteLine($"[{string.Join(", ", newArray)}]");
-*/
 
+string[] newArray = A.Split(" ");
 
+Console.WriteLine($"[{string.Join(", ", newArray).TrimEnd(',', ' ')}]");
 
 
 
